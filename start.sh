deeff77772d7fafd665d5e1ddf87c166d26322da
pengfei.miao <pengfei.miao@thoughtworks.com>
2024-12-11 10:45:47 +0800

    feat: update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
+pid=$(jps | grep "wechat-bc" | awk '{print $1}')
+
+if [ -n "$pid" ]; then
+    echo "Killing process with PID: $pid"
+    kill -9 "$pid"
+else
+    echo "Process not found."
+fi
+
 nohup java -jar ./libs/wechat-bc-1.1.9-SNAPSHOT.jar > output.log 2>&1 &
 tail -f output.log
