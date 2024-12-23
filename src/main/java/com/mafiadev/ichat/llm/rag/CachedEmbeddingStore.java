ca8f79d57aa387e032a84b349f21e9f60dd29b68
pengfei.miao <pengfei.miao@thoughtworks.com>
2024-12-23 11:03:36 +0800

    feat: update rag update & delete policy

--- src/main/java/com/mafiadev/ichat/llm/rag/CachedEmbeddingStore.java
+++ src/main/java/com/mafiadev/ichat/llm/rag/CachedEmbeddingStore.java
@@ -2,12 +2,12 @@ package com.mafiadev.ichat.llm.rag;
 
 import com.mafiadev.ichat.llm.GptService;
 import dev.langchain4j.data.document.Document;
-import dev.langchain4j.data.document.DocumentLoader;
+import dev.langchain4j.data.document.Metadata;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
 
-import java.util.Collections;
+import java.util.Map;
 
 public class CachedEmbeddingStore {
     private static volatile InMemoryEmbeddingStore<TextSegment> instance;
@@ -23,8 +23,14 @@ public class CachedEmbeddingStore {
         return instance;
     }
 
-    public static void addDocument(String text) {
+    public static void addDocument(String text, Map<String, Object> metadata) {
         InMemoryEmbeddingStore<TextSegment> embeddingStore = getInstance();
-        EmbeddingStoreIngestor.ingest(Collections.singletonList(Document.document(text)), embeddingStore);
+        Document document = Document.document(text, Metadata.from(metadata));
+        EmbeddingStoreIngestor.ingest(document, embeddingStore);
+    }
+
+    public static void removeDocument(String key, String value) {
+        InMemoryEmbeddingStore<TextSegment> embeddingStore = getInstance();
+        embeddingStore.removeAll(it -> value.equals(((Metadata) it).getString(key)));
     }
 }
--- src/main/java/com/mafiadev/ichat/task/RagRefreshTask.java
+++ src/main/java/com/mafiadev/ichat/task/RagRefreshTask.java
@@ -3,6 +3,7 @@ package com.mafiadev.ichat.task;
 import com.mafiadev.ichat.crawler.WeiBoCrawler;
 import com.mafiadev.ichat.llm.rag.CachedEmbeddingStore;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import static com.mafiadev.ichat.task.TaskTrigger.TASK_EXEC;
@@ -10,7 +11,11 @@ import static com.mafiadev.ichat.task.TaskTrigger.TASK_EXEC;
 public class RagRefreshTask {
     public RagRefreshTask() {
         Runnable task = () -> {
-            CachedEmbeddingStore.addDocument(WeiBoCrawler.crawlWeiboTops());
+            CachedEmbeddingStore.removeDocument("from", "weibo");
+            CachedEmbeddingStore.addDocument(WeiBoCrawler.crawlWeiboTops(), new HashMap<String, Object>() {{
+                put("from", "weibo");
+                put("keyword", "微博热搜");
+            }});
         };
 
         TASK_EXEC.scheduleAtFixedRate(task, 0, 1, TimeUnit.HOURS);
